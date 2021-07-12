3974a7ddf9d48c3cad0f390db591c42dfa0a70f6
mill <ph.mill@gmail.com>
2021-07-12 10:28:07 +0200

    API with NestJS #4. Error handling and data validation

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,11 +4,13 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Put,
   UseGuards,
 } from '@nestjs/common';
 import JwtAuthenticationGuard from 'src/authentication/jwt-authentication.guard';
+import FindOneParams from 'src/utils/findOneParams';
 import CreatePostDto from './dto/createPost.dto';
 import UpdatePostDto from './dto/updatePost.dto';
 import PostsService from './posts.service';
@@ -23,7 +25,7 @@ export default class PostsController {
   }
 
   @Get(':id')
-  getPostById(@Param('id') id: string) {
+  getPostById(@Param() { id }: FindOneParams) {
     return this.postsService.getPostById(Number(id));
   }
 
@@ -33,10 +35,18 @@ export default class PostsController {
     return this.postsService.createPost(post);
   }
 
-  @Put(':id')
-  async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+  @Patch(':id')
+  async updatePost(
+    @Param() { id }: FindOneParams,
+    @Body() post: UpdatePostDto,
+  ) {
     return this.postsService.updatePost(Number(id), post);
   }
+
+  // @Put(':id')
+  // async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+  //   return this.postsService.updatePost(Number(id), post);
+  // }
 
   @Delete(':id')
   async deletePost(@Param('id') id: string) {
